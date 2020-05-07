e0e41be908ce76f12cc4cfc08003faefbcdb8d8b
shravz16 <48133320+shravz16@users.noreply.github.com>
2020-05-07 11:50:26 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -2,14 +2,13 @@ import docx
 from docx.shared import Pt
 from docx.shared import Inches
 import os
-from docx2pdf import convert
+
 from tkinter import *
 from tkinter import filedialog
 import tkinter as tk 
-import comtypes.client
+
 import win32com.client
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import inch
+
 import string
 import time
 import sys
@@ -30,7 +29,7 @@ def stop():
     root.destroy()
 def select():
     gg=filedialog.askopenfilename(initialdir=defaultLoc,title="choose a docx file",filetypes=(("docx files","*docx")   ,("docx files","*docx")))
-    message = tk.Label(root, text="" ,bg="#7DCEA0"  ,fg="#7DCEA0"  ,width=50  ,height=3,font=('caliber', 30, 'bold')) 
+    message = tk.Label(root, text="" ,bg="#7DCEA0"  ,fg="#7DCEA0"  ,width=int((0.0462962)*screen_height)  ,height=3,font=('caliber', int((0.027777)*screen_height), 'bold')) 
     message.place(x=(0.447916*screen_width), y=(0.64814814*screen_height))
     
     doc = docx.Document(gg)
@@ -66,7 +65,7 @@ def start(doc,doc1,name):
     doc.SaveAs(defaultLoc+'/'+name, FileFormat=wdFormatPDF)
     doc.Close()
     word.Quit()
-    message = tk.Label(root, text="Successfully converted to "+name+".pdf" ,bg="green"  ,fg="white"  ,width=50  ,height=3,font=('caliber', 30, 'bold')) 
+    message = tk.Label(root, text="Successfully converted to "+name+".pdf" ,bg="green"  ,fg="white"  ,width=int((0.0462962)*screen_height)  ,height=3,font=('caliber', int((0.027777)*screen_height), 'bold')) 
     message.place(x=(0.207916*screen_width), y=(0.44814814*screen_height))
 
     
@@ -78,9 +77,9 @@ def start(doc,doc1,name):
 
 
 root.bind("<Escape>",exit) 
-message = tk.Label(root, text="Document to Handwriting" ,bg="#5DADE2"  ,fg="white"  ,width=50  ,height=3,font=('caliber', 30, 'bold')) 
-message.place(x=(0.20916*screen_width),y=20)
-space = tk.Button(root, text="click to start", command=select  ,fg="white"  ,bg="#2E86C1"  ,width=20  ,height=3, activebackground = "white" ,font=('times', 15, ' bold '))
+message = tk.Label(root, text="Document to Handwriting" ,bg="#5DADE2"  ,fg="white"  ,width=int((0.0462962)*screen_height)  ,height=3,font=('caliber', 30, 'bold')) 
+message.place(x=(0.20916*screen_width),y=(0.0185185)*screen_height)
+space = tk.Button(root, text="click to start", command=select  ,fg="white"  ,bg="#2E86C1"  ,width=int((0.0185185)*screen_height)  ,height=3, activebackground = "white" ,font=('times', 15, ' bold '))
 space.place(x=(0.447916*screen_width), y=(0.44814814*screen_height))
 close=tk.Button(root,text="X",command=stop,fg="white"  ,bg="red"  ,width=4  ,height=2, activebackground = "white" ,font=('times', 10, ' bold '))
 close.place(x=(0.963541*screen_width),y=(0.00925925*screen_height))
